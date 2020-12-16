ebeac2e9b2f27f886922fb4e035a1538beb29b2c
jjohnston55 <jjohnston55@users.noreply.github.com>
2020-12-16 13:53:52 -0700

    Created base page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { Col, Collapse, Row } from 'antd';
+
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="App">
+			<Row justify='space-around'>
+				<Col span={10}>
+					<Collapse>
+						<Collapse.Panel header="List Products" key="1"></Collapse.Panel>
+						<Collapse.Panel header="Get Product" key="2"></Collapse.Panel>
+						<Collapse.Panel header="Create Product" key="3"></Collapse.Panel>
+						<Collapse.Panel header="Update Product" key="4"></Collapse.Panel>
+						<Collapse.Panel header="Delete Product" key="5"></Collapse.Panel>
+					</Collapse>
+				</Col>
+				<Col span={10}>
+					<Collapse>
+						<Collapse.Panel header="List Categories" key="1"></Collapse.Panel>
+						<Collapse.Panel header="Get Category" key="2"></Collapse.Panel>
+						<Collapse.Panel header="Create Category" key="3"></Collapse.Panel>
+						<Collapse.Panel header="Update Category" key="4"></Collapse.Panel>
+						<Collapse.Panel header="Delete Category" key="5"></Collapse.Panel>
+					</Collapse>
+				</Col>
+			</Row>
+		</div>
+  	);
 }
 
 export default App;
